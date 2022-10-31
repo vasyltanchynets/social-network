95dbf74850cadcdcb8dd1078d70414adda90a8f0
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2022-10-31 12:41:10 +0200

    ref

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,14 +7,27 @@ const MyPosts = (props) => {
         <Post message={post.message} likesCount={post.likesCount} />
     ));
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea name="" id="" cols="30" rows="5"></textarea>
+                <textarea
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="5"
+                    ref={newPostElement}
+                ></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={styles.posts}>{postsElements}</div>
         </div>
