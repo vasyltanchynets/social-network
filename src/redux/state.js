f1aa371e30d817d76e80d79366992920670bac7f
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2022-11-01 22:54:47 +0200

    callback rerenderEntireTree refactoring

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+    console.log("State changed");
+};
 
 let state = {
     profilePage: {
@@ -24,7 +26,7 @@ let state = {
     },
 };
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -36,9 +38,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 };
 
 export default state;
