405d93a1af8a88d9460ec5a8409b899f510fda47
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2022-11-05 01:46:09 +0200

    action creator, action type to constant

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import {
+    addPostActionCreator,
+    updateNewPostTextActionCreator,
+} from "./../../../redux/state";
 
 const MyPosts = (props) => {
     let postsElements = props.postsData.map((post) => (
@@ -10,12 +14,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({ type: "ADD-POST" });
+        props.dispatch(addPostActionCreator());
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
+        props.dispatch(updateNewPostTextActionCreator(text));
     };
 
     return (
