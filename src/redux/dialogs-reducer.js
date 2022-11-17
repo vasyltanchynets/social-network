d28fff208d2f9a5774d737bfa5873801708fdb4e
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2022-11-17 22:03:17 +0200

    redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,23 @@
 const SEND_MESSAGE = "SEND-MESSAGE";
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogsData: [
+        { id: 1, name: "Sofia" },
+        { id: 2, name: "Solomia" },
+        { id: 3, name: "Marta" },
+        { id: 4, name: "Ksenia" },
+    ],
+    messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "Hey" },
+        { id: 3, message: "Hello" },
+        { id: 4, message: "Welcome" },
+    ],
+    newMessageBody: "",
+};
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             let body = state.newMessageBody;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,15 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+let initialState = {
+    postsData: [
+        { id: 1, message: "Hi, how are you?", likesCount: 98 },
+        { id: 2, message: "It's my first post", likesCount: 25 },
+    ],
+    newPostText: "Network",
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
